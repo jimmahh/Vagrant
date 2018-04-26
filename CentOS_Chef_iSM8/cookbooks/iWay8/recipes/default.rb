41bba9f4f9e89594059137ba0b93b27b358a43e4
Chris Abernethy <chris_abernethy@ibi.com>
2018-04-26 09:45:19 +0100

    Install/config mDNS

--- CentOS_Chef_iSM8/cookbooks/iWay8/recipes/default.rb
+++ CentOS_Chef_iSM8/cookbooks/iWay8/recipes/default.rb
@@ -3,6 +3,8 @@
 # Recipe:: default
 #
 
+# Bootstrapping bits (update yum repository in the main =)
+
 # Update yum ahead of next steps
 execute "yum update" do
   command "yum update"
--- CentOS_Chef_iSM8/cookbooks/iWay8/recipes/mDNS.rb
+++ CentOS_Chef_iSM8/cookbooks/iWay8/recipes/mDNS.rb
@@ -4,3 +4,19 @@
 #
 
 # Steps to configure mDNS
+# ----------------------------------------------------------
+
+# A zero-congig mDNS package
+package "avahi" do
+  action :install
+end
+
+# A package for command-line tools for mDNS browsing and publishing
+package "avahi-tools" do
+  action :install
+end
+
+# Registers avahi as an auto-start background service
+service "avahi-daemon" do
+  action [:enable, :start]
+end
